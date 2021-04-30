d9afde24db6010fcb71ab4ae9ea9067f0dbae80c
Hasancan Boga <hasancanboga@gmail.com>
2021-05-01 00:04:01 +0300

    final

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,6 @@
 
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-        ? './'
-        // ? '/pokemon-vue-tailwind/' // this is the 'GitHub Pages' path. normally we use "/" when deploying on root of a normal server (apache, nginx or node server). but it is bugged so we need to use "./" instead.
+        ? '/pokemon-vue-tailwind/' // usage explained in my vue.js deployment notes.
         : '/'
 }
