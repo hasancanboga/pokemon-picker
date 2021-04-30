1b029a88c2d91d263f14306d1ffa97ded94fb867
Hasancan Boga <hasancanboga@gmail.com>
2021-04-30 23:23:57 +0300

    publicPath test 1

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@
 
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-        ? '/pokemon-vue-tailwind/'
+        ? './'
+        // ? '/pokemon-vue-tailwind/' // this is the 'GitHub Pages' path. normally we use "/" when deploying on root of a normal server (apache, nginx or node server). but it is bugged so we need to use "./" instead.
         : '/'
 }
